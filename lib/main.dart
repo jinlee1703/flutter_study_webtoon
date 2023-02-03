ba7a0a95617321ff263c5fc93fdcf94bd0b9d7ee
Jinwoo Lee <binarywooo@gmail.com>
2023-02-03 15:01:29 +0900

    feat: implement app_bar and data fetching function

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:webtoon/screens/home_screen.dart';
+import 'package:webtoon/services/api_service.dart';
 
 void main() {
+  ApiService().getTodaysToons();
   runApp(App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container(),
+      home: HomeScreen(),
     );
   }
 }
