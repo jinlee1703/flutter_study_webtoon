8d0913e5dfe7382b865c28e8cf382ae5af20e8a5
Jinwoo Lee <binarywooo@gmail.com>
2023-02-03 19:17:01 +0900

    feat: implement get_webtoon_info_api

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
-import 'package:webtoon/models/webtoon_model.dart';
+import '../models/webtoon_model.dart';
+import '../models/webtoon_detail_model.dart';
+import '../models/webtoon_episode_model.dart';
 
 class ApiService {
   static const String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
@@ -17,6 +18,30 @@ class ApiService {
         webtoonInstances.add(WebtoonModel.fromJson(webtoon));
       }
       return webtoonInstances;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse('$baseUrl/$id');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fronJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getRLatestEpisodesById(String id) async {
+    List<WebtoonEpisodeModel> episodesInstances = [];
+    final url = Uri.parse('$baseUrl/$id/episodes');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        episodesInstances.add(WebtoonEpisodeModel.fronJson(episode));
+      }
+      return episodesInstances;
     }
     throw Error();
   }
